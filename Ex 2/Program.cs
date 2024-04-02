5259c9e8d8002c1b4c7d5822932edf4ec402e0b3
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-04-02 22:01:23 +0300

    Add project files.

---
+++ Ex 2/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Ex 2
+Scrieti un program care va modela un autoturism. Un autoturism va avea o
+marca (string), un numar de inmatriculare (string), precum si o
+capacitate cilindrica(int).
+Autoturismul va avea:
+ Campurile corespunzatoare caracteristicilor autoturismului.
+ Un constructor care va initializa campurile acestuia
+ O metoda “Tiparire” care va afisa pe consola descrierea acestuia sub forma : “marca,
+numarDeInmatriculare, capacitateCilindrica”.
+Folositi: clase, campuri, metode, modificatorii de acces adecvati, comentarii
+xml pentru metodele publice.
+Creeati doua obiecte de tipul autoturism in functia “Main” si apelati-le
+metodele “Tipareste”*/
+
+Car Ford = new Car("Transit", "123443", 2200);
+
+
+Car Opel = new Car("Corsa", "543765", 1000);
+
+
+Console.WriteLine($"The {Ford.model}, with number {Ford.number}, has {Ford.capCil} cc");
+Console.WriteLine($"The {Opel.model}, with number {Opel.number}, has {Opel.capCil} cc");
+
+
+class Car
+{
+    public string model;
+    public string number;
+    public int capCil;
+
+    public Car(string modelName, string modelnumber, int modelcapCil)
+    {
+        model = modelName;
+        number = modelnumber;
+        capCil = modelcapCil;
+    }
+}
+
---
+++ LAb 6 Ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o
+metoda care va calcula volumul acestuia.
+Folositi o clasa care va modela un dulap.
+ Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+ Cele trei campuri vor fi initializate cu ajutorul constructorului
+ Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si
+afisati rezultatul.*/
+
+var Dulap = new Dulap();
+Dulap.SetVolume();
+
+Console.WriteLine("O volume do dulap é : {0}", Dulap.GetVolume());
+
+Console.ReadLine();
+
+class Dulap
+{
+    private int lungimel;
+    private int latime;
+    private int inaltime;
+
+    public void SetVolume()
+    {
+        lungimel = 6;
+        latime = 4;
+        inaltime = 2;
+    }
+    public int GetVolume()
+    {
+        return lungimel * latime * inaltime;
+    }
+}
